6a7312d1d97f63f3e6c1d5c7cc7d1dc4128d9437
Cristhina1 <U22327233@utp.edu.pe>
2025-09-17 18:44:05 -0500

    verificar

--- src/main/java/com/sistemaFacturacion/Mambo/model/Factura.java
+++ src/main/java/com/sistemaFacturacion/Mambo/model/Factura.java
@@ -25,66 +25,9 @@ public class Factura {
 
     @ManyToOne
     @JoinColumn(name = "cliente_id", nullable = false)
-    private Cliente cliente;
+    private cliente cliente;
     @ManyToOne
     @JoinColumn(name = "producto_id", nullable = false)
     private Producto producto;
-<<<<<<< HEAD
 
-    public Factura() {
-    }
-
-    public Factura(String numeroFactura, LocalDate fecha, Double total, Cliente cliente, Producto producto) {
-        this.numeroFactura = numeroFactura;
-        this.fecha = fecha;
-        this.total = total;
-        this.cliente = cliente;
-        this.producto = producto;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getNumeroFactura() {
-        return numeroFactura;
-    }
-
-    public void setNumeroFactura(String numeroFactura) {
-        this.numeroFactura = numeroFactura;
-    }
-
-    public LocalDate getFecha() {
-        return fecha;
-    }
-
-    public void setFecha(LocalDate fecha) {
-        this.fecha = fecha;
-    }
-
-    public Double getTotal() {
-        return total;
-    }
-
-    public void setTotal(Double total) {
-        this.total = total;
-    }
-
-    public Cliente getCliente() {
-        return cliente;
-    }
-
-    public void setCliente(Cliente cliente) {
-        this.cliente = cliente;
-    }
-
-    public Producto getProducto() {
-        return producto;
-    }
-
-    public void setProducto(Producto producto) {
-        this.producto = producto;
-    }
-=======
->>>>>>> Isabel
 }
