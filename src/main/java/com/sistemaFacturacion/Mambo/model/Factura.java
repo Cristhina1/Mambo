92d60507771057e889e1bd3009f3afe09580c3d9
Paulo <paulooliverabautista@hmail.com>
2025-09-16 20:25:24 -0500

    Se agrego el modelo Factura

---
+++ src/main/java/com/sistemaFacturacion/Mambo/model/Factura.java
@@ -0,0 +1,84 @@
+package com.sistemaFacturacion.Mambo.model;
+
+import jakarta.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "facturas")
+public class Factura {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String numeroFactura;
+
+    @Column(nullable = false)
+    private LocalDate fecha;
+
+    @Column(nullable = false)
+    private Double total;
+
+    @ManyToOne
+    @JoinColumn(name = "cliente_id", nullable = false)
+    private cliente cliente;
+    @ManyToOne
+    @JoinColumn(name = "producto_id", nullable = false)
+    private Producto producto;
+
+    public Factura() {
+    }
+
+    public Factura(String numeroFactura, LocalDate fecha, Double total, cliente cliente, Producto producto) {
+        this.numeroFactura = numeroFactura;
+        this.fecha = fecha;
+        this.total = total;
+        this.cliente = cliente;
+        this.producto = producto;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getNumeroFactura() {
+        return numeroFactura;
+    }
+
+    public void setNumeroFactura(String numeroFactura) {
+        this.numeroFactura = numeroFactura;
+    }
+
+    public LocalDate getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(LocalDate fecha) {
+        this.fecha = fecha;
+    }
+
+    public Double getTotal() {
+        return total;
+    }
+
+    public void setTotal(Double total) {
+        this.total = total;
+    }
+
+    public cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public Producto getProducto() {
+        return producto;
+    }
+
+    public void setProducto(Producto producto) {
+        this.producto = producto;
+    }
+}
