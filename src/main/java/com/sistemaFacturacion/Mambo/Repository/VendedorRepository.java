596ad7594f698d8081d7ab0cc673c45774b8ca7a
Jose Bojorquez <u22311398@utp.edu.pe>
2025-10-12 01:51:21 -0500

    Vendedor Repo

---
+++ src/main/java/com/sistemaFacturacion/Mambo/Repository/VendedorRepository.java
@@ -0,0 +1,8 @@
+package com.sistemaFacturacion.Mambo.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.sistemaFacturacion.Mambo.model.Vendedor;
+
+public interface VendedorRepository extends JpaRepository<Vendedor, Long> {
+
+}
