0682d71522ca066d4ea61eb302144283909f0b4f
Jose Bojorquez <u22311398@utp.edu.pe>
2025-11-08 18:54:38 -0500

    fix:vistas de admin 2

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { ListUsuariosComponent } from './admin/list-usuarios/list-usuarios';
+import { ProductosComponent } from './admin/productos/productos';
+import { ReporteComponent } from './admin/reporte/reporte';
+import { VendedoresComponent } from './admin/vendedores/vendedores';
+
+export const routes: Routes = [
+  { path: '', redirectTo: 'admin/home', pathMatch: 'full' },
+  { path: 'admin/list-usuarios', component: ListUsuariosComponent },
+  { path: 'admin/productos', component: ProductosComponent },
+  { path: 'admin/reporte', component: ReporteComponent },
+  { path: 'admin/vendedores', component: VendedoresComponent }
+];
