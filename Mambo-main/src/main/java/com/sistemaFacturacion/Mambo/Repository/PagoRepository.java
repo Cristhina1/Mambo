5aef9804d1837b23d958854298d99d62b08769b1
Cristhina1 <U22327233@utp.edu.pe>
2025-11-19 19:51:28 -0500

    complet

--- Mambo-main/src/main/java/com/sistemaFacturacion/Mambo/Repository/PagoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.sistemaFacturacion.Mambo.Repository;
-
-import com.sistemaFacturacion.Mambo.model.pago;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PagoRepository extends JpaRepository<pago, Long> {
-}
